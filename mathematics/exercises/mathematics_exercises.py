a6335e6c8b313dcd749e15bcf89a90e9786c9536
Raimo Kivi <raimokivi5@gmail.com>
2023-10-23 13:19:24 +0300

    math exercise solution

--- mathematics/exercises/mathematics_exercises.py
+++ mathematics/exercises/mathematics_exercises.py
@@ -51,7 +51,8 @@ def area_of_a_circle(radius: float) -> float:
 def area_of_an_equilateral_triangle(side_length: float) -> int:
     """Calculate and return the area of an equilateral triangle."""
     # Write your code here
-    triangle_area = (side_length ** 2 * (3 ** 0.5)) / 4
+    s = 3 * side_lenght / 2
+    triangle_area = nath.sqrt(s * ())
     return triangle_area
 
 
@@ -78,22 +79,30 @@ def calculate_cathetus_length(a: int, c: int) -> float:
 def time_converter(seconds: int) -> str:
     """Convert time in seconds to hours and minutes."""
     # Write your code here
+    minutes = seconds // 60
+    hours = minutes // 60
+    minutes = minutes % 60
     return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
 
 
 def student_helper(angle: int) -> str:
     """Return the sine and cosine of the given angle in degrees."""
     # Write your code here
+    sine = round(math.sin(math.radians(angle)), 1)
+    cosine = round(math.cos(math.radians(angle)), 1)
+    
     return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
 
 
 def greetings(n: int) -> str:
     """Return a string that contains "Hey" n times."""
     # Write your code here
+    lots_of_heys = "Hey" * n
     return lots_of_heys
 
 
 def adding_numbers(num_a: int, num_b: int) -> str:
     """Return given numbers added together as a string."""
     # Write your code here
+    string_numbers = str(num_a) + str(num_b)
     return string_numbers
